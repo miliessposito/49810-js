697721372979d4ed85c99ee6610ba8e9fda919e4
milagros esposito <millie_esposito@hotmail.com>
2024-01-16 13:51:03 -0300

    preentrega-3

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@
 
 //}
 
-//FIN DE LA PREENTREGA 1 ----------------------------------------------------
+ //FIN DE LA PREENTREGA 1 ----------------------------------------------------
 
 //EJEMPLOS
 /*Variable
@@ -112,7 +112,7 @@ while(true){
     }
 }*/
 
-
+/*PRE ENTREGA 2 -------------------------------------------------------------------------
 let productos= [
     {
         nombre: "Top bordado",
@@ -160,3 +160,70 @@ productos.sort(function(a,b){
 });
 
 console.log(productos); 
+
+FIN DE LA PREENTREGA 2 ----------------------------------------------------*/
+
+
+//PRE ENTREGA 3 -----------------------------------------------------------------
+
+//genero el objeto constructor
+document.addEventListener('DOMContentLoaded', function () {
+    class Producto {
+        constructor(id, nombre, precio) {
+            this.id = id;
+            this.nombre = nombre;
+            this.precio = precio;
+        }
+    }
+
+    const producto1 = new Producto(1, "Conjunto Magda", 7000);
+    const producto2 = new Producto(2, "Conjunto Alaska", 6500);
+    const producto3 = new Producto(3, "Conjunto Helena", 8000);
+    const producto4 = new Producto(4, "Conjunto Emily", 9000);
+    const producto5 = new Producto(5, "Conjunto Brisa", 5700);
+    const producto6 = new Producto(6, "Conjunto Melody", 6500);
+    const producto7 = new Producto(7, "Conjunto Leila", 9900);
+    const producto8 = new Producto(8, "Conjunto Selena", 7500);
+
+    //genero un array vacio para almacenar los productos
+    const carritoDeCompras = [];
+
+    //almaceno en una variable lo que llamo de html
+    const SECTION_CONT_SHOP = document.getElementById("sectionContenedorShop");
+
+    // agrego al carrito y almaceno en localstorage
+    function agregarAlCarrito(producto) {
+        carritoDeCompras.push(producto);
+        alert(`${producto.nombre} se ha añadido al carrito.`);
+        console.log(`Producto añadido al carrito: ${producto.nombre} - Precio: $${producto.precio}`);
+        localStorage.setItem('carrito', JSON.stringify(carritoDeCompras));
+    }
+
+    function crearTarjeta(producto) {
+        const tarjeta = document.createElement("div");
+        tarjeta.classList.add("cardShop");
+
+        tarjeta.innerHTML = `
+            <img src="../assets/img/art${producto.id}.jpg" alt="">
+            <div>
+                <h1>${producto.nombre}</h1>
+                <p>$${producto.precio}</p>
+                <button class="agregarAlCarrito">Agregar al carrito</button>
+            </div>
+        `;
+
+        // evento boton
+        const botonCarrito = tarjeta.querySelector(".agregarAlCarrito");
+        botonCarrito.addEventListener("click", function () {
+            agregarAlCarrito(producto);
+        });
+
+        return tarjeta;
+    } 
+
+    // bucle foreach
+    [producto1,producto2,producto3,producto4,producto5,producto6,producto7,producto8].forEach(function (producto) {
+        const tarjeta = crearTarjeta(producto);
+        SECTION_CONT_SHOP.appendChild(tarjeta);
+    });
+});
